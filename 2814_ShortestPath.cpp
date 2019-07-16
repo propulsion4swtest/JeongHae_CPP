eae9146d5c729b5dd620d0a90018d0a3fa66581e
ScareCrow1992 <49647538+ScareCrow1992@users.noreply.github.com>
2019-07-16 22:25:38 +0900

    Update 2814_ShortestPath.cpp

--- 2814_ShortestPath.cpp
+++ 2814_ShortestPath.cpp
@@ -17,19 +17,32 @@ bool flag[10];
 
 int N, M;
 
-/*
-int DFS2(int node, int length)
-{
+//0~N 번의 시작점을 일일이 호출해주어야한다.
+//더이상 갈곳이 없다 : length 반환
+//이동할 곳이 있다 : 이동할 경로중 최대 이동거리 반환
 
+
+int DFS(int node, int length)
+{
 	flag[node] = true;
 
 	int ret = 0;
+	int maxPath = node;
 
 	for (int i = 0; i < N; i++)
 	{
 		if (path[node][i] == true && flag[i] == false)
 		{
-			int tmp = DFS2(i, length + 1);
+			int tmp = 0;
+			if (length <= startCache[node][i])
+				tmp = length + cache[node][i];
+			else
+			{
+				tmp = DFS(i, length + 1);
+				startCache[node][i] = length;
+				cache[node][i] = tmp - length;
+			}
+
 			ret = max(ret, tmp);
 		}
 	}
@@ -37,13 +50,82 @@ int DFS2(int node, int length)
 	if (ret == 0)
 		ret = length;
 
-	
+	//더이상 갈곳이 없으면 지금까지 이동한 값을 반환한다.
 	flag[node] = false;
 	ret = max(ret, length);
 	return ret;
 }
 
-*/
+
+int main(void)
+{
+	int T;
+	cin >> T;
+	for (int testCase = 0; testCase < T; testCase++)
+	{
+		cin >> N >> M;
+
+
+		for (int row = 0; row < N; row++)
+		{
+			for (int cul = 0; cul < N; cul++)
+			{
+				path[row][cul] = false;
+			}
+		}
+
+		int a, b;
+		for (int i = 0; i < M; i++)
+		{
+			cin >> a >> b;
+			path[a - 1][b - 1] = true;
+			path[b - 1][a - 1] = true;
+		}
+
+		int ret = 0;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int row = 0; row < N; row++)
+			{
+				for (int cul = 0; cul < N; cul++)
+				{
+					startCache[row][cul] = -1;
+					cache[row][cul] = -1;
+				}
+
+				flag[row] = false;
+			}
+
+			int tmp = DFS(i, 0);
+			ret = max(ret, tmp);
+		}
+
+		cout << "#" << testCase + 1 << " " << ret + 1 << endl;
+	}
+
+	return 0;
+}
+
+
+/*
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+
+bool path[10][10];
+
+//방향성을 주기 위해 2차 배열을 사용하였다.
+int cache[10][10];
+int startCache[10][10];
+
+
+bool flag[10];
+
+int N, M;
 
 //0~N 번의 시작점을 일일이 호출해주어야한다.
 //더이상 갈곳이 없다 : length 반환
@@ -135,7 +217,7 @@ int main(void)
 
 	return 0;
 }
-
+*/
 
 
 /*
