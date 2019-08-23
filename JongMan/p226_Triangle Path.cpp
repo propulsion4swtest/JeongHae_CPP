c748669bbd2e0b3ed06cbe7e63a5d79cb118498b
ScareCrow1992 <49647538+ScareCrow1992@users.noreply.github.com>
2019-08-23 22:29:18 +0900

    Update p226_Triangle Path.cpp

--- JongMan/p226_Triangle Path.cpp
+++ JongMan/p226_Triangle Path.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<string.h>
 
 using namespace std;
 
-vector<vector<int>> Triangle;
-vector<vector<int>> Cache;
+int Triangle[110][110];
+int Cache[110][110];
 int N;
 
 /*
@@ -17,45 +17,56 @@ int N;
 2 7 5 9 4
 */
 
-int maxValue = -1;
-void Solve(int x, int y, int score)
+
+int Solve(int x, int y, int score)
 {
 	if(y == N - 1)
 	{
-		Cache[y][x] = max(Cache[y][x], score);
-		maxValue = max(maxValue, Cache[y][x]);
-		return;
+		Cache[y][x] = score;
+		return score;
 	}
 
-	if(Cache[y][x] >= score)
-		return;
-	else
-		Cache[y][x] = score;	
+	if(Cache[y][x] >= 0)
+		return score + Cache[y][x];
 
-	Solve(x, y + 1, Triangle[y + 1][x] + score);
+	int ret = -1;
+	int tmp1, tmp2;
 
-	Solve(x + 1, y + 1, Triangle[y + 1][x + 1] + score);
+	tmp1 = Solve(x, y + 1, Triangle[y + 1][x] + score);
+	tmp2 = Solve(x + 1, y + 1, Triangle[y + 1][x + 1] + score);
+
+	ret = max(tmp1, tmp2);
+
+	Cache[y][x] = ret - score;
+
+	return ret;
 }
+
+
 
 
 
 int main(void)
 {
-	cin >> N;
-	Triangle.assign(N, vector<int>(N, -1));
-	Cache.assign(N, vector<int>(N, -1));
-	int input;
-	for(int row = 0; row<N; row++)
+	int T;
+	cin >> T;
+	for (int testCase = 0; testCase < T; testCase++)
 	{
-		for(int cul = 0; cul < row + 1; cul++)
+		cin >> N;
+		memset(Cache, -1, sizeof(Cache));
+		int input;
+		for (int row = 0; row < N; row++)
 		{
-			cin >> input;
-			Triangle[row][cul] = input;
+			for (int cul = 0; cul < row + 1; cul++)
+			{
+				cin >> input;
+				Triangle[row][cul] = input;
+			}
 		}
-	}
 
-	Solve(0,0,Triangle[0][0]);
-	cout<<maxValue<<endl;
+		int value = Solve(0, 0, Triangle[0][0]);
+		cout << value << endl;
+	}
 
 	return 0;
 }
