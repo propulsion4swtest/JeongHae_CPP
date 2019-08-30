436603f740bd5f78d1a44131a78f064e0bcbb5dc
ScareCrow1992 <49647538+ScareCrow1992@users.noreply.github.com>
2019-08-30 23:00:10 +0900

    Update 2294(slow).cpp

--- B_Baekjoon/DP/2294(slow).cpp
+++ B_Baekjoon/DP/2294(slow).cpp
@@ -11,17 +11,16 @@ int DFS(int arrSize, int score, int index)
 		return arrSize;
 	if (score > K)
 		return 2000000000;
-	
 	if (cache[index][score] != -1)
 		return cache[index][score] + arrSize;
-		
+	if (index == N)
+		return 2000000000;
 	int tmp, ret;
 	ret = 2000000000;
-	for (int i = index; i < N; i++)
-	{		
-		tmp = DFS(arrSize + 1, score + coins[i], i);
-		ret = min(ret, tmp);
-	}
+	tmp = DFS(arrSize + 1, score + coins[index], index);
+	ret = min(ret, tmp);
+	tmp = DFS(arrSize, score, index + 1);
+	ret = min(ret, tmp);
 	cache[index][score] = ret - arrSize;
 	return ret;
 }
@@ -41,14 +40,17 @@ int main(void)
 
 
 /*
-이 방법은 느리고, 메모리를 많이 차지하지만
+중복조합을 베이스로 하여
+순서에 따른 중복 연산을 막는 연산이다
+가령
 1.1.1.2
 1.1.2.1
 1.2.1.1
 2.1.1.1
-이 4가지를
+
+4가지 케이스를 다 찾는 이전의 방식에 비해
 
 1.1.1.2
 
-한가지로 줄여서 탐색한다는 점에 있다.
+딱 한번만 찾는다
 */
