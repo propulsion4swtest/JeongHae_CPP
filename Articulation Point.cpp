fb0cb64b46f27078deb6bcebf947209838f9cf42
ScareCrow1992 <49647538+ScareCrow1992@users.noreply.github.com>
2019-12-31 18:47:57 +0900

    Update Articulation Point.cpp

--- Articulation Point.cpp
+++ Articulation Point.cpp
@@ -1,53 +1,77 @@
-//https://jason9319.tistory.com/119
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
 
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#define MAX_N 10000
 using namespace std;
-int n, m, disc[MAX_N + 1], cut[MAX_N + 1], ans, d, a, b;
-vector<vector<int>> vt;
-int dfs(int here, bool r) {
-    disc[here] = ++d;
-    int ret = disc[here];
-    int child = 0;
-    for (int there : vt[here]) {
-        if (!disc[there]) {
-            child++;
-            int df = dfs(there, 0);
-            if (!r && df >= disc[here])
-                cut[here] = true;
-            ret = min(ret, df);
-        }
-        else
-            ret = min(ret, disc[there]);
-    }
-    if (r && child > 1)
-        cut[here] = true;
-    return ret;
-}
-int main() {
-    scanf("%d%d", &n, &m);
-    vt.resize(n + 1);
-    for (int i = 0; i < m; i++) {
-        scanf("%d%d", &a, &b);
-        vt[a].push_back(b);
-        vt[b].push_back(a);
-    }
 
-    for (int i = 1; i <= n; i++) {
-        if (!disc[i]) {
-            dfs(i, 1);
-        }
-    }
+vector<vector<int>> edges; //edges[i] : i번 노드와 연결된 노드들의 번호 목록
+vector<int> disc;
+vector<bool> articulation_flag;
+vector<int> articulation_points;
+int cDisc;
+int cnt = 0;
 
-    for (int i = 1; i <= n; i++)
-        if (cut[i])
-            ans++;
-    printf("%d\n", ans);
-    for (int i = 1; i <= n; i++)
-        if (cut[i])
-            printf("%d ", i);
-    return 0;
+int DFS(int here, bool isStart) {
+	int child = 0;
+	disc[here] = cDisc;
+	cDisc++;
+	int ret = disc[here];
+	for (int i = 0; i < edges[here].size(); i++) {
+		int there = edges[here][i];
+		if (disc[there] == -1) {
+			child++;
+			int tmp = DFS(there, false);
+			if (!isStart && (disc[here] <= tmp)) {
+				if (articulation_flag[here] == false) {
+					articulation_flag[here] = true;
+					articulation_points.push_back(here);
+				}			
+			}
+			ret = min(ret, tmp);
+		}
+		else {
+			ret = min(ret, disc[there]);
+		}
+
+	}
+
+	if (isStart && child >= 2) {
+		if (articulation_flag[here] == false) {
+			articulation_flag[here] = true;
+			articulation_points.push_back(here);
+		}
+	}
+	return ret;
 }
 
+
+int main(void) {
+	int N, K;
+	scanf("%d %d", &N, &K);
+	edges.assign(N, vector<int>());
+	disc.assign(N, -1);
+	articulation_flag.assign(N, false);
+	int from, to;
+	for (int i = 0; i < K; i++) {
+		scanf("%d %d", &from, &to);
+		from--; to--;
+		edges[from].push_back(to);
+		edges[to].push_back(from);
+	}
+
+	cDisc = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (disc[i] == -1)
+			DFS(i, true);
+	}
+	
+	sort(articulation_points.begin(), articulation_points.end());
+
+	printf("%d\n", articulation_points.size());
+	for (int i = 0; i < articulation_points.size(); i++)
+		printf("%d ", articulation_points[i] + 1);
+	printf("\n");
+
+	return 0;
+}
